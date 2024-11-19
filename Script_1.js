8bb069f3da1bb9ec266d3fb0aae30e02ce3c0b2b
mohammedismail03 <mohammedismail03@gmail.com>
2024-11-19 13:33:20 +0000

    DS3 - [adding Script_1.js]

--- Script_1.js
+++ Script_1.js
@@ -6,9 +6,8 @@ setShadowDOM(true);
 navigateTo("https://signin.ft.cayuse.com/login?tenant_id=e8dd2da8-4499-48dd-ab70-7308be56378d&redirect_uri=https%3A%2F%2Fesiriuspt.ft.cayuse.com%2Frrm%2Fapi%2Fauthentication%3Foriginal_uri%3Dhttps%3A%2F%2Fesiriuspt.ft.cayuse.com%2Frrm%2Fanimal-inventory%2Fexport-requests&response_type=CODE&state&nonce&auth_error=false");
 addSyntheticDPL("{ds}/Login_Credentials.csv",0);
 
-var y = "{ds}/Login_Credentials.csv";
-store in locker "y" ="{ds}/Login_Credentials.csv";
-retrieve from locker x="y";
+store in locker y ="{ds}/Login_Credentials.csv";
+retrieve from locker x=y;
 console.log(x);
 
 // var x=y.length;
